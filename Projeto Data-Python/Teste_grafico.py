97796af0714764734a5ad9b72d231be103d6e903
Junior Braga <cbc.junior@hotmail.com>
2025-08-07 22:02:05 -0300

    Aulas de python e aulas de html

---
+++ Projeto Data-Python/Teste_grafico.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+
